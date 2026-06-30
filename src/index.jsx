f57e746b141985d5413bac2f1d881a1a0dc92def
jupiter7741 <anhdt0410@gmail.com>
2026-06-30 16:46:44 +0700

    Build: Setup Vite React

--- src/index.jsx
+++ src/index.jsx
@@ -200,4 +200,8 @@ function NavItem({ icon, label, active, onClick }) {
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
